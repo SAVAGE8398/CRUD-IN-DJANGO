469a3af8ecd3782a3f7b6d78642576e0a6531307
Sagar <sagarkaushik1998@gmail.com>
2022-05-23 12:41:46 +0530

    Registered User through Database with the use of Post Method

--- Operations/models.py
+++ Operations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    FirstNameF = models.CharField(max_length=255)
+    LastNameF = models.CharField(max_length=255)
+    EmailF = models.CharField(max_length=255)
+    PasswordF = models.CharField(max_length=255)
+    CpasswordF = models.CharField(max_length=255)
--- Operations/views.py
+++ Operations/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        data=User(FirstNameF=request.POST["txtfname"],LastNameF=request.POST["txtlname"],EmailF=request.POST["txtemail"],PasswordF=request.POST["txtpassword"])
+        data.save()
     return render(request,'index.html')
